a8f58071807787eb3b71d99e2b9c2152dc43ed3b
Mohamad-PC <mohamadek@edu.hac.ac.il>
2021-12-13 00:19:16 +0200

    Third-commit

--- FaceMaskDetectionTrain.py
+++ FaceMaskDetectionTrain.py
@@ -21,11 +21,15 @@ import numpy as np
 import os
 
 INIT_LR = 1e-4
-EPOCHS = 10
+EPOCHS = 1000
 BS = 32
 
-DIRECTORY = r".\TrainingDataset"
+DIRECTORY = r".\dataset"
 CATEGORIES = ["withMask", "WithOutMask"]
+
+# grab the list of images in our dataset directory, then initialize
+# the list of data (i.e., images) and class images
+print("[INFO] loading images...")
 
 data = []
 labels = []
@@ -33,13 +37,13 @@ labels = []
 for category in CATEGORIES:
     path = os.path.join(DIRECTORY, category)
     for img in os.listdir(path):
-        img_path = os.path.join(path, img)
-        image = load_img(img_path, target_size=(224, 224))
-        image = img_to_array(image)
-        image = preprocess_input(image)
+    	img_path = os.path.join(path, img)
+    	image = load_img(img_path, target_size=(224, 224))
+    	image = img_to_array(image)
+    	image = preprocess_input(image)
 
-        data.append(image)
-        labels.append(category)
+    	data.append(image)
+    	labels.append(category)
 
 # perform one-hot encoding on the labels
 lb = LabelBinarizer()
@@ -49,23 +53,23 @@ labels = to_categorical(labels)
 data = np.array(data, dtype="float32")
 labels = np.array(labels)
 
-(X_train, X_test, y_train, y_test) = train_test_split(data, labels,
-                                                      test_size=.2, stratify=labels, random_state=42)
+(trainX, testX, trainY, testY) = train_test_split(data, labels,
+	test_size=0.20, stratify=labels, random_state=42)
 
 # construct the training image generator for data augmentation
 aug = ImageDataGenerator(
-    rotation_range=20,
-    zoom_range=0.15,
-    width_shift_range=0.2,
-    height_shift_range=0.2,
-    shear_range=0.15,
-    horizontal_flip=True,
-    fill_mode="nearest")
+	rotation_range=20,
+	zoom_range=0.15,
+	width_shift_range=0.2,
+	height_shift_range=0.2,
+	shear_range=0.15,
+	horizontal_flip=True,
+	fill_mode="nearest")
 
 # load the MobileNetV2 network, ensuring the head FC layer sets are
 # left off
 baseModel = MobileNetV2(weights="imagenet", include_top=False,
-                        input_tensor=Input(shape=(224, 224, 3)))
+	input_tensor=Input(shape=(224, 224, 3)))
 
 # construct the head of the model that will be placed on top of the
 # the base model
@@ -83,31 +87,33 @@ model = Model(inputs=baseModel.input, outputs=headModel)
 # loop over all layers in the base model and freeze them so they will
 # *not* be updated during the first training process
 for layer in baseModel.layers:
-    layer.trainable = False
+	layer.trainable = False
 
 # compile our model
-#print("[INFO] compiling model...")
+print("[INFO] compiling model...")
 opt = Adam(lr=INIT_LR, decay=INIT_LR / EPOCHS)
 model.compile(loss="binary_crossentropy", optimizer=opt,
-              metrics=["accuracy"])
+	metrics=["accuracy"])
 
 # train the head of the network
-#print("[INFO] training head...")
-H = model.fit(X_train, y_train, epochs=200, batch_size=32, validation_data=(X_test, y_test),
-              validation_steps=len(X_test) // 32)
+print("[INFO] training head...")
+# train the head of the network
+print("[INFO] training head...")
+H = model.fit(trainX,trainY,epochs=EPOCHS,batch_size=BS)
 
 # make predictions on the testing set
-#print("[INFO] evaluating network...")
-predIdxs = model.predict(X_test, batch_size=BS)
+print("[INFO] evaluating network...")
+predIdxs = model.predict(testX, batch_size=BS)
 
 # for each image in the testing set we need to find the index of the
 # label with corresponding largest predicted probability
 predIdxs = np.argmax(predIdxs, axis=1)
 
 # show a nicely formatted classification report
-#print(classification_report(y_test.argmax(axis=1), predIdxs,
-#                            target_names=lb.classes_))
+print(classification_report(testY.argmax(axis=1), predIdxs,
+	target_names=lb.classes_))
 
 # serialize the model to disk
-#print("[INFO] saving mask detector model...")
+print("[INFO] saving mask detector model...")
 model.save("mask_detector.model", save_format="h5")
+

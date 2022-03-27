3fc96ee2d9329e0be05f97df16b2f72f9aea5d06
Mohamad Ektelat <71977816+mohamadektelat@users.noreply.github.com>
2022-03-27 15:47:48 +0300

    some changes

---
+++ threads/PicturesThread.py
@@ -0,0 +1,27 @@
+import cv2
+from qt_core import *
+import numpy as np
+import os
+
+imagesFolder = r"savedImages"
+
+
+class PicturesThread(QThread):
+    change_pixmap_signal = Signal(np.ndarray)
+
+    def __init__(self):
+        super().__init__()
+        self._run_flag = True
+
+    def run(self):
+        img_lst = os.listdir(imagesFolder)  # returns list
+        # while self._run_flag:
+        for img in img_lst:
+            print(img)
+            cv_img = cv2.imread(imagesFolder + '/' + img, cv2.IMREAD_COLOR)
+            self.change_pixmap_signal.emit(cv_img)
+
+    def stop(self):
+        """Sets run flag to False and waits for thread to finish"""
+        self._run_flag = False
+        self.wait()

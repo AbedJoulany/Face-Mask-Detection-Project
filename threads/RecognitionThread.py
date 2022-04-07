71cec3c571c23e133b33e087454f6d4e00c1de3b
AbedJoulany <59060418+AbedJoulany@users.noreply.github.com>
2022-04-08 01:03:03 +0300

    making pool thread to execute the recognition and adding the pictutres to the ui

--- threads/RecognitionThread.py
+++ threads/RecognitionThread.py
@@ -11,19 +11,20 @@ class RecognitionThread(QThread):
     change_pixmap_signal = Signal(np.ndarray)
 
 
-    def __init__(self):
+    def __init__(self, frame):
         super().__init__()
         self._run_flag = True
+        self.frame = frame
         self.sfr = SimpleFacerec().load_encoding_images("controller/images/")
 
-    def run(self, frame):
-        face_locations, face_names = self.sfr.detect_known_faces(frame)
+    def run(self):
+        face_locations, face_names = self.sfr.detect_known_faces(self.frame)
         for face_loc, name in zip(face_locations, face_names):
             # y1, x2, y2, x1 = face_loc[0], face_loc[1], face_loc[2], face_loc[3]
             # cv2.putText(frame, name, (x1, y1 - 10), cv2.FONT_HERSHEY_DUPLEX, 1, (0, 0, 200), 2)
             # cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 200), 4)
             facesfilename = FacesImagesFolder + "/image_" + name + ".jpg"
-            cv2.imwrite(facesfilename, frame)
+            cv2.imwrite(facesfilename, self.frame)
 
     def stop(self):
         """Sets run flag to False and waits for thread to finish"""
